466b595f04f956f0f2a52dd19cf7fec05260dd85
lolcheng <1397799304@qq.com>
2026-09-17 13:57:44 +0800

    stop tracking ignored build artifacts

--- lift_leg_trajopt_cpp/build/CMakeFiles/seg_trajopt_cpp.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for seg_trajopt_cpp.
-# This may be replaced when dependencies are built.
--- trajopt_cpp/build/CMakeFiles/trajopt_cpp.dir/src/objectives/regularization_objective.cpp.o.d
+++
@@ -1,5 +0,0 @@
-CMakeFiles/trajopt_cpp.dir/src/objectives/regularization_objective.cpp.o: \
- /home/yizhe/trajopt/trajopt_cpp/src/objectives/regularization_objective.cpp \
- /usr/include/stdc-predef.h \
- /home/yizhe/trajopt/trajopt_cpp/include/objectives/regularization_objective.hpp \
- /home/yizhe/trajopt/trajopt_cpp/include/objective_base.hpp
